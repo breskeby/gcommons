341171291aa4f9e4762b77a884d9c847e735b9a3
Evgeny Goldin <evgenyg@gmail.com>
2011-03-21 11:56:52 +0200

    NetBean: file listing log improvements

--- src/main/groovy/com/goldin/gcommons/beans/NetBean.groovy
+++ src/main/groovy/com/goldin/gcommons/beans/NetBean.groovy
@@ -164,7 +164,7 @@ class NetBean extends BaseBean
 
                     getLog( this ).info( "[$globPattern] - [$files.length] file${ general.s( files.length ) }" )
                     List<GFTPFile> gfiles = files.collect { FTPFile file -> new GFTPFile( file, remotePath ) }
-                    if ( getLog( this ).isDebugEnabled()) { getLog( this ).debug( general.stars( gfiles*.path ))}
+                    if ( getLog( this ).isDebugEnabled()) { getLog( this ).debug( "\n" + general.stars( gfiles*.path ))}
 
                     result.addAll( gfiles )
                 }
